e52e1530d99d6c0ffbc40a41200585bb3f074c99
HankTech <HankTech@gmail.com>
2022-03-02 12:51:52 -0400

    add new rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,8 @@ module.exports = {
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
-        'no-undef': 'off'
+        'no-undef': 'off',
+        'no-use-before-define': 'off'
       }
     }
   ]
